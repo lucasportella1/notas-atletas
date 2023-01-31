51c6b2566ea9f33ef1ab40b1c892a260804d4a4b
lucasportella1 <123984687+lucasportella1@users.noreply.github.com>
2023-01-30 23:45:18 -0300

    Update notas-atletas.js
    Finalização do projeto

--- notas-atletas.js
+++ notas-atletas.js
@@ -1,1 +1,35 @@
+let atletas = [
+ {
+   nome: "Cesar Abascal",
+   notas: [10, 9.34, 8.42, 10, 7.88]
+ },
+ {
+   nome: "Fernando Puntel",
+   notas:  [8, 10, 10, 7, 9.33]
+ },
+ {
+   nome: "Daiane Jelinsky",
+   notas: [7, 10, 9.5, 9.5, 8]
+ },
+ {
+   nome: "Bruno Castro",
+   notas: [10, 10, 10, 9, 9.5]
+ }
+];
 
+function obterEstatistica(){
+for (let i = 0; i < atletas.length; i++) {
+  console.log("Atleta: " + atletas[i].nome)
+  console.log("Notas Obtidas: "+ atletas[i].notas.sort())
+  function obterMedia () {
+    let soma = 0
+    atletas[i].notas.sort().slice(1,4).forEach(function(nota){
+      soma = soma + nota
+    })
+    return soma / (atletas[i].notas.length - 2)
+}    
+    console.log("Nota Válida: " + obterMedia())
+    console.log(" ")
+}
+}
+console.log(obterEstatistica())
